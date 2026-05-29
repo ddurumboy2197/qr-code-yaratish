b550aa9b4ac78b616b19a696aea52149676b7210
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:27:54 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,18 @@
+import qrcode
+
+def qrcode_generator(data, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+data = "https://www.example.com"
+filename = "qrcode.png"
+qrcode_generator(data, filename)
